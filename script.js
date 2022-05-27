52d552b63935517212defad9c1aea220c4b74829
Kevillin Santos <kevillin_santos@hotmail.com>
2022-05-27 11:27:18 -0300

    Requisitos 2 e 3

---
+++ script.js
@@ -0,0 +1,18 @@
+// const divs = document.getElementsByClassName('color');
+
+// function corIgual () {
+//   for (let index = 0; index < divs.length; index += 1){
+//     for (let IndexInt = 1; IndexInt < divs.length; IndexInt += 1){
+//       if (divs[index].style.backgroundColor === divs[IndexInt].style.backgroundColor) {
+//         return true;
+//       }
+//       return false;
+//     }
+//   }
+// };
+
+// corIgual();
+document.getElementsByClassName('color')[0].style.backgroundColor = 'black';
+document.getElementsByClassName('color')[1].style.backgroundColor = 'yellow';
+document.getElementsByClassName('color')[2].style.backgroundColor = 'green';
+document.getElementsByClassName('color')[3].style.backgroundColor = 'purple';
